6bea80015ed0f4cd08d3426e36847ddafd7f1a83
webdesign4alll <190048@tkmce.ac.in>
2022-06-25 14:07:00 +0530

    changed style

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('user/index', { title: 'Athena T summit' });
+});
+router.get('/summit',(req,res,next)=>{
+
+  res.render('user/summit');
+});
+
+router.post('/summitform',function(req,res){
+  console.log("submitting form is working here at summit form");
+  console.log(req.body.delegateonename);
+
 });
 
 module.exports = router;
